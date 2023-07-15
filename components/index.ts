d544b95964f24afbee5635dd61803b6dda7ae134
deshan68 <arundeshan>
2023-07-15 15:00:49 +0530

    hero, footer, and filter manufacturer

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
